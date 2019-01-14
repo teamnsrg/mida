a7b38ac7ca6f43ab812916c741673655bf5e25cf
Paul Murley <paul@murley.io>
2019-01-14 14:01:23 -0600

    experimentation

--- main.go
+++ main.go
@@ -1,16 +1,56 @@
 package main
 
-import(
-    log "github.com/sirupsen/logrus"
+import (
+	"context"
+	log "github.com/Sirupsen/logrus"
+	"github.com/chromedp/cdproto/debugger"
+	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/chromedp/runner"
+	"github.com/teamnsrg/chromedp"
+	"time"
 )
 
-func main () {
-    log.Info("Hello World")
+func main() {
 
-    
+	cxt, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
+	c, err := chromedp.New(cxt, chromedp.WithRunnerOptions(
+		runner.Flag("remote-debugging-port", 8088)))
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	err = c.Run(cxt, chromedp.CallbackFunc("Network.requestWillBeSent", func(param interface{}, handler *chromedp.TargetHandler) {
+		data := param.(*network.EventRequestWillBeSent)
+		log.Info(data.Request.URL)
+	}))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.Run(cxt, chromedp.CallbackFunc("Debugger.scriptParsed", func(param interface{}, handler *chromedp.TargetHandler) {
+		data := param.(*debugger.EventScriptParsed)
+		result, _ := debugger.GetScriptSource(data.ScriptID).Do(cxt, handler)
+		log.Debug(result)
+
+	}))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.Run(cxt, chromedp.Navigate("https://www.cnn.com"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.Run(cxt, chromedp.Sleep(10*time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.Shutdown(cxt)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-
-
-
