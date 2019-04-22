40df74b69a21083de1bbc813e0823c53b0af8745
Paul Murley <paul@murley.io>
2019-04-22 14:20:19 -0500

    fix path in deploy script

--- scripts/deploy_new_version.sh
+++ scripts/deploy_new_version.sh
@@ -6,11 +6,11 @@ GOOS=darwin GOARCH=amd64 go build -o mida_darwin_amd64
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     LINUX_HASH=$(sha256sum mida_linux_amd64)
     DARWIN_HASH=$(sha256sum mida_darwin_amd64)
-    SETUP_SCRIPT_HASH=$(sha256sum setup.py)
+    SETUP_SCRIPT_HASH=$(sha256sum scripts/setup.py)
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     LINUX_HASH=$(gsha256sum mida_linux_amd64)
     DARWIN_HASH=$(gsha256sum mida_darwin_amd64)
-    SETUP_SCRIPT_HASH=$(gsha256sum setup.py)
+    SETUP_SCRIPT_HASH=$(gsha256sum scripts/setup.py)
 fi
 
 > sha256sums.txt
@@ -20,7 +20,7 @@ echo ${SETUP_SCRIPT_HASH} >> sha256sums.txt
 
 scp mida_linux_amd64 files.mida.sprai.org:.mida.linux.amd64.tmp
 scp mida_darwin_amd64 files.mida.sprai.org:.mida.darwin.amd64.tmp
-scp setup.py files.mida.sprai.org:.setup.tmp
+scp scripts/setup.py files.mida.sprai.org:.setup.tmp
 scp sha256sums.txt files.mida.sprai.org:sha256sums.txt
 ssh -t files.mida.sprai.org "
   sudo mv .mida.darwin.amd64.tmp /var/www/files.mida.sprai.org/mida_darwin_amd64;
