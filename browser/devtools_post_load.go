cd0583cf46b84bf10a668c0aa895f786d254ffad
Paul Murley <paul@murley.io>
2020-12-17 11:01:00 -0600

    update gremlinsjs to run indefinitely after it starts

--- browser/devtools_post_load.go
+++ browser/devtools_post_load.go
@@ -117,7 +117,7 @@ func basicInteraction(cxt context.Context, taskLog *logrus.Logger, wg *sync.Wait
 // significantly more aggressive than the basic interaction.
 func runGremlinsJS(cxt context.Context, taskLog *logrus.Logger, wg *sync.WaitGroup) {
 	var err error
-	gremlinsApplet := `javascript: (function() { function callback() { gremlins.createHorde({ species: [gremlins.species.clicker(),gremlins.species.toucher(),gremlins.species.formFiller(),gremlins.species.scroller(),gremlins.species.typer()], mogwais: [gremlins.mogwais.alert(),gremlins.mogwais.fps(),gremlins.mogwais.gizmo()], strategies: [gremlins.strategies.distribution()] }).unleash(); } var s = document.createElement("script"); s.src = "https://unpkg.com/gremlins.js"; if (s.addEventListener) { s.addEventListener("load", callback, false); } else if (s.readyState) { s.onreadystatechange = callback; } document.body.appendChild(s); })()`
+    gremlinsApplet := `javascript: (function() { function callback() { gremlins.createHorde({ species: [gremlins.species.clicker(),gremlins.species.toucher(),gremlins.species.formFiller(),gremlins.species.scroller(),gremlins.species.typer()], mogwais: [gremlins.mogwais.alert(),gremlins.mogwais.fps(),gremlins.mogwais.gizmo()], strategies: [gremlins.strategies.distribution(), gremlins.strategies.allTogether({ nb: 100000 })] }).unleash(); } var s = document.createElement("script"); s.src = "https://unpkg.com/gremlins.js"; if (s.addEventListener) { s.addEventListener("load", callback, false); } else if (s.readyState) { s.onreadystatechange = callback; } document.body.appendChild(s); })()`
 
 	err = chromedp.Run(cxt, chromedp.ActionFunc(func(cxt context.Context) error {
 		var bytes = new([]byte)
